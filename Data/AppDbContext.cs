decfc88a2968bd8b90d45d1af1355728473bcbb0
Daniel Lopes <daniel.dxdb@gmail.com>
2022-02-04 14:53:41 -0300

    First commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModel>? Todos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# API using MVC, EF Core and .NET 6
+
+A simple Todo API using SQLite and EF Core to learn MVC.
